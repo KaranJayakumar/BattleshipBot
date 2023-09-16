13dae0d9814da61aaebcc42e2c8e853d867d96f0
vjy02 <vyojoh@gmail.com>
2023-09-16 15:05:22 +1000

    Added updateBoard

--- Battleship.py
+++ Battleship.py
@@ -2,4 +2,23 @@ import random
 def ShipLogic(round, yourMap, yourHp, enemyHp, p1ShotSeq, p1PrevHit, storage):
     x = random.randint(1,10)
     y = random.randint(1,10)
+
+    curEnemyBoard = updateBoard(p1ShotSeq, p1PrevHit) 
+
     return [x,y], storage
+
+
+def updateBoard(p1ShotSeq, p1PrevHit):
+    board = [["." for _ in range(10)] for _ in range(10)]
+    p1Missed = []
+    for coord in p1ShotSeq:
+        if coord not in p1PrevHit:
+            p1Missed.append(coord)
+
+    for coord in p1PrevHit:
+        board[coord[0]][coord[1]] = "H"
+
+    for coord in p1Missed:
+        board[coord[0]][coord[1]] = "M"
+
+    return board 
