cf1596420189adc86d5590dc77c4fcbae7f13fe9
jeangerbeer <142638632+jeangerbeer@users.noreply.github.com>
2023-09-17 11:49:04 +1000

    Update ShipPos.py

--- ShipPos.py
+++ ShipPos.py
@@ -1,20 +1,25 @@
 def getShipPos():
-    '''
-    THIS IS THE LIST OF SHIPS
-    [5,3,3,2,2] 
-    That is: 
-    1x 5 long
-    2x 3 long
-    2x 2 long
-
-    Your ships must satisfy this 
-    '''
-
-    # due to a bug we have the indexing of ships are 0-9
     
-    shipPos = [[(3,1), (4,1),(5,1)], 
-                [(2,1),(2,2),(2,3),(2,4),(2,5)], 
-                [(7,7),(8,7)] , 
-                [(0,9), (1,9), (2,9)], 
-                [(5,9), (6,9)]]
+    #counter centre attacks
+    shipPos = [[(6,0), (6,1), (6,2)], 
+                [(0,0), (0,1), (0,2), (0,3), (0,4)], 
+                [(1,8), (2,8)] , 
+                [(7,5), (8,5), (9,5)], 
+                [(6,8), (6,9)]]
     return shipPos
+
+    #diagonal line counter    
+    shipPos = [[(1,2), (1,3),(1,4)], 
+                [(3,8),(4,8),(5,8),(6,8),(7,8)], 
+                [(6,0),(6,1)] , 
+                [(2,3), (2,4), (2,5)], 
+                [(8,4), (9,4)]]
+    return shipPos
+
+    #chequerboard counter
+    shipPos = [[(6,7), (6,8),(6,9)], 
+                [(3,3),(4,3),(5,3),(6,3),(7,3)], 
+                [(2,0),(2,1)] , 
+                [(9,7), (9,8), (9,9)], 
+                [(4,5), (5,5)]]
+    return shipPos,m 
