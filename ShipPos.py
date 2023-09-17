ad763e189075da538bbe30d35023915663a3bc54
jeangerbeer <142638632+jeangerbeer@users.noreply.github.com>
2023-09-17 11:55:54 +1000

    Update ShipPos.py

--- ShipPos.py
+++ ShipPos.py
@@ -1,10 +1,8 @@
 def getShipPos():
     
-    #counter centre attacks
     shipPos = [[(6,0), (6,1), (6,2)], 
                 [(0,0), (0,1), (0,2), (0,3), (0,4)], 
                 [(1,8), (2,8)] , 
                 [(7,5), (8,5), (9,5)], 
                 [(6,8), (6,9)]]
     return shipPos
-
